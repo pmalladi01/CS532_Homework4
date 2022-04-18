e9656ecdcad8b5ac09798aa090285419ec9012df
pmalladi01 <98726057+pmalladi01@users.noreply.github.com>
2022-04-17 19:30:57 -0500

    added main function.

--- Homework4.c
+++ Homework4.c
@@ -21,9 +21,34 @@ struct Node
     char endtime[100];
     char **args1;
 };
+
 static struct Node *process_list = NULL;
 #define WAITING 0
 #define RUNNING 1
 #define COMPLETED 2
 #define RUNIT 3
 int mxx = 1;
+
+void main(int *argc, int **args)
+{
+    process_list = NULL;
+    char line[1024];
+    char *argv[100];
+    int n = atoi(args[1]);
+    while (1)
+    {
+        scheduler(n);             
+        printf("Enter Command> "); 
+        gets(line);               
+        printf("\n");
+        parse(line, argv);              
+        if (strcmp(argv[0], "exit") == 0) 
+            exit(0);
+        if (strcmp(argv[0], "submit") == 0)
+            append(argv);
+        scheduler(n); 
+        if (strcmp(argv[0], "showjobs") == 0)
+            show();
+        if (strcmp(argv[0], "submithistory") == 0)
+            showall();
+    }
