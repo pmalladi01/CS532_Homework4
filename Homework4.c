940ace33ab9c0aa3943d8a8bb32b7c66a5b45b49
pmalladi01 <98726057+pmalladi01@users.noreply.github.com>
2022-04-17 19:49:17 -0500

    added parse function.

--- Homework4.c
+++ Homework4.c
@@ -105,3 +105,19 @@ void append(char **args)
     last->next = new_node;
     return;
 }
+
+void parse(char *line, char **argv)
+{
+
+    while (*line != '\0')
+    {
+        while (*line == ' ' || *line == '\t' || *line == '\n')
+            *line++ = '\0';
+
+        *argv++ = line;
+        while (*line != '\0' && *line != ' ' &&
+               *line != '\t' && *line != '\n')
+            line++;
+    }
+    *argv = '\0';
+}
