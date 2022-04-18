187a8143ef5b5c34213568217c76850937348c82
pmalladi01 <98726057+pmalladi01@users.noreply.github.com>
2022-04-17 19:27:49 -0500

    Implemented the node structure and defined global variables.

--- Homework4.c
+++ Homework4.c
@@ -9,3 +9,21 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+struct Node
+{
+    int jobid;
+    int status;
+    struct Node *next;
+    int prid;
+    char starttime[100];
+    char arg1[100];
+    char arg2[100];
+    char endtime[100];
+    char **args1;
+};
+static struct Node *process_list = NULL;
+#define WAITING 0
+#define RUNNING 1
+#define COMPLETED 2
+#define RUNIT 3
+int mxx = 1;
